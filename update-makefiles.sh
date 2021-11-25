41f1d117fed5481df64e46ff20952e07dc1f85d9
alanndz <alanndz@qmail.id>
2021-11-25 10:13:32 +0000

    Adapt for bianca
    Signed-off-by: alanndz <alanndz@qmail.id>

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.arrow:hardware/arrow/interfaces" \
+  "vendor.bianca:hardware/bianca/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
